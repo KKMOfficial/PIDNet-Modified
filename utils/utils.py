d7ed9191799e381bce4a24152366e3ca6c4fe80c
KKMOfficial <89024919+KKMOfficial@users.noreply.github.com>
2024-08-12 17:00:34 +0330

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -51,7 +51,7 @@ class FullModel(nn.Module):
     # print(f"input shape is {inputs.shape}")
     # print(f"outputs[-2] shape is {outputs[-2].shape}")
     # print(f"outputs[-1] shape is {outputs[-1].shape}")
-    if (writer is not None) and (i_iter%10==1):
+    if (writer is not None) and (i_iter%20==1):
         writer.add_images(f"Pre-Enter/images-epoch{epoch}", torchvision.utils.make_grid(inputs)[None,:,:,:], global_step=i_iter//10)
         writer.add_images(f"Pre-Enter/labels-epoch{epoch}", torchvision.utils.make_grid(labels.reshape((labels.shape[0],1,labels.shape[1],labels.shape[2])))[None,:,:,:], global_step=i_iter//10)
         writer.add_images(f"Post-Infer/Border-epoch{epoch}", torchvision.utils.make_grid(outputs[-1])[None,:,:,:], global_step=i_iter//10)
